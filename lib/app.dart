796df9ddd47bec9c270384f30059d888f184d45f
Jason Adesanya <jason.adesanya@users.noreply.github.com>
2026-08-10 23:59:21 +0100

    Restore incomplete Sender profiles on session recovery

--- lib/app.dart
+++ lib/app.dart
@@ -5,9 +5,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'app/account/bloc/account_bloc.dart';
 import 'app/authentication/bloc/auth_bloc.dart';
 import 'app/authentication/view/index_page.dart';
+import 'app/authentication/view/add_details.dart';
 import 'app/history/bloc/history_bloc.dart';
 import 'app/onboarding/view/onboarding.dart';
 import 'app/sender_mobile/sender_mobile_home.dart';
+import 'app/sender_mobile/sender_profile_authority.dart';
 import 'app/send_package/bloc/send_package_bloc.dart';
 import 'app/support/bloc/support_bloc.dart';
 import 'utils/app_state/app_state.dart';
@@ -94,13 +96,49 @@ class _SessionGate extends StatelessWidget {
       builder: (context, state) {
         switch (state.currentState) {
           case AppState.authenticated:
-            return const SenderMobileHome(previewAuthEnabled: true);
+            return const _SenderRestorationGate();
           case AppState.unauthenticated:
             return const OnboardingView();
           case AppState.unknownSessionState:
           default:
             return const IndexPage();
         }
+      },
+    );
+  }
+}
+
+class _SenderRestorationGate extends StatefulWidget {
+  const _SenderRestorationGate();
+
+  @override
+  State<_SenderRestorationGate> createState() => _SenderRestorationGateState();
+}
+
+class _SenderRestorationGateState extends State<_SenderRestorationGate> {
+  late Future<SenderProfileAuthoritySnapshot> _profile;
+
+  @override
+  void initState() {
+    super.initState();
+    _profile = SenderProfileAuthority().load('session.restore');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<SenderProfileAuthoritySnapshot>(
+      future: _profile,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const IndexPage();
+        }
+        if (snapshot.hasError) {
+          return const IndexPage();
+        }
+        final data = snapshot.data?.data ?? const <String, dynamic>{};
+        final name = '${data['displayName'] ?? data['fullName'] ?? data['name'] ?? ''}'.trim();
+        if (name.isEmpty) return const AddDetailsView();
+        return const SenderMobileHome(previewAuthEnabled: true);
       },
     );
   }
