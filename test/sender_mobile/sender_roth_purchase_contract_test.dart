14849ed3f2467c36bad782c5658ff53135c41e51
Jason Adesanya <ayojason600@gmail.com>
2026-08-12 22:20:05 +0100

    Guard Business and Roth in unified Sender release

--- test/sender_mobile/sender_roth_purchase_contract_test.dart
+++ test/sender_mobile/sender_roth_purchase_contract_test.dart
@@ -14,4 +14,18 @@ void main() {
     expect(source, isNot(contains('rothCredit: amount')));
     expect(source, isNot(contains("collection('senderWallets').doc")));
   });
+
+  test('Sender release retains Business and Roth together', () {
+    final home = File('lib/app/sender_mobile/sender_mobile_home.dart')
+        .readAsStringSync();
+    final routing = File('lib/web_platform_routing.dart').readAsStringSync();
+    final wallet =
+        File('lib/app/sender_mobile/sender_wallet.dart').readAsStringSync();
+
+    expect(home, contains("import '../business/business_access_view.dart';"));
+    expect(home, contains('BusinessAccessView'));
+    expect(routing, contains("canonicalPath: '/send/business'"));
+    expect(wallet, contains("title: 'Buy Roth'"));
+    expect(wallet, contains("httpsCallable('createWalletTopUp')"));
+  });
 }
